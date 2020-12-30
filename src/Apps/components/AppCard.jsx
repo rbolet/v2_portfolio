947747fa3c4582c37acab300cb47feeb31157d28
rbolet <rapha.bolet@gmail.com>
2020-12-29 16:19:12 -0800

    getting data from app data json file

--- src/Apps/components/AppCard.jsx
+++ src/Apps/components/AppCard.jsx
@@ -6,33 +6,40 @@ import { faExternalLinkAlt } from "@fortawesome/free-solid-svg-icons";
 import styles from "../styles/Apps.module.css";
 
 export default function AppCard(props) {
-  const { img, appName, subTitle } = props;
+  const { img, appName, subtitle, bulletPoints, gitHubRoute, sscRoute } = props;
   return (
     <div className={styles.appCard}>
       <div className={styles.imageWrapper}>
-        <Image
-          className={styles.appImage}
-          src="/assets/gradebook.gif"
-          layout="fill"
-          objectFit="cover"
-          objectPosition="50% top"
-        />
+        {img ? (
+          <Image
+            className={styles.appImage}
+            src={img}
+            layout="fill"
+            objectFit="cover"
+            objectPosition="50% top"
+          />
+        ) : (
+          <div style={{ height: "100%", width: "100%", backgroundColor: "black" }} />
+        )}
         <div className={styles.imageCaption}>
-          <h3>Gradebook</h3>
-          <p>A simple tool to track student grades</p>
+          <h3>{appName}</h3>
+          <p>{subtitle}</p>
         </div>
       </div>
       <div className={styles.appCardContent}>
-        <li>Full-stack: React.js | Node/Express | MySQL</li>
-        <li>Fully mobile responsive using Bootstrap 4</li>
+        {bulletPoints?.length && bulletPoints.map((string) => <li>{string}</li>)}
       </div>
       <div className={styles.appCardFooter}>
-        <Link href="https://gradebook.slightlyskewedcreations.com">
-          <FontAwesomeIcon icon={faGithub} size="3x" />
-        </Link>
-        <Link href="https://gradebook.slightlyskewedcreations.com">
-          <FontAwesomeIcon icon={faExternalLinkAlt} size="3x" />
-        </Link>
+        {gitHubRoute && (
+          <Link href={`https://${gitHubRoute}.slightlyskewedcreations.com`}>
+            <FontAwesomeIcon icon={faGithub} size="3x" />
+          </Link>
+        )}
+        {sscRoute && (
+          <Link href={`https://${sscRoute}.slightlyskewedcreations.com`}>
+            <FontAwesomeIcon icon={faExternalLinkAlt} size="3x" />
+          </Link>
+        )}
       </div>
     </div>
   );
--- src/pages/apps.js
+++ src/pages/apps.js
@@ -2,6 +2,7 @@ import Head from "next/head";
 import Header from "../shared/Header";
 import AppCard from "../Apps/components/AppCard";
 import styles from "../Apps/styles/Apps.module.css";
+import appList from "../Apps/data/appdata.json";
 
 export default function Apps() {
   return (
@@ -14,9 +15,17 @@ export default function Apps() {
       <main className={styles.beforeFold}>
         <section className="apps">
           <div className={styles.appsContainer}>
-            <AppCard />
-            <AppCard />
-            <AppCard />
+            {appList?.length &&
+              appList.map((data) => (
+                <AppCard
+                  img={data.img}
+                  appName={data.appName}
+                  subtitle={data.subtitle}
+                  bulletPoints={data.bulletPoints}
+                  gitHubRoute={data.gitHubRoute}
+                  sscRoute={data.sscRoute}
+                />
+              ))}
           </div>
         </section>
       </main>
