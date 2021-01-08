54b63c0785eab79f5353c9b5c0ce4a75fe55fe63
rbolet <rapha.bolet@gmail.com>
2021-01-07 22:15:57 -0800

    trying to update preview image

--- src/pages/index.js
+++ src/pages/index.js
@@ -15,29 +15,23 @@ export default function Home() {
           content="Meet Rapha, Full Stack JavaScript Developer: React | React Native | Node/Express"
         />
 
-        <meta property="og:type" content="website" />
-        <meta property="og:url" content="https://metatags.io/" />
+        {/* <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://metatags.io/" /> */}
         <meta property="og:title" content="Who Is Rapha?" />
         <meta
           property="og:description"
           content="Meet Rapha, Full Stack JavaScript Developer: React | React Native | Node/Express"
         />
-        <meta
-          property="og:image"
-          content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png"
-        />
+        <meta property="og:image" content="./assets/Preview.png" />
 
         <meta property="twitter:card" content="summary_large_image" />
-        <meta property="twitter:url" content="https://metatags.io/" />
+        {/* <meta property="twitter:url" content="https://metatags.io/" /> */}
         <meta property="twitter:title" content="Who Is Rapha?" />
         <meta
           property="twitter:description"
           content="Meet Rapha, Full Stack JavaScript Developer: React | React Native | Node/Express"
         />
-        <meta
-          property="twitter:image"
-          content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png"
-        />
+        <meta property="twitter:image" content="./assets/Preview.png" />
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
       <Header />
